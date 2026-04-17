077da849b4dfb0ecd77aaaf1e673d512deb4e06a
fernovoa97 <94500661+fernovoa97@users.noreply.github.com>
2026-04-17 13:59:10 -0500

    Add route for leads index

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\VentaController;
 
+Route::get('/leads', [LeadController::class, 'index'])->name('leads.index');
+
 Route::get('/', function () {
     return auth()->check()
         ? redirect()->route('dashboard')
