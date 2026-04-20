8ede4832a3d6b078237a4dc18fa3b5662882a4c0
Fernando Novoa <fernovoa97@gmail.com>
2026-04-20 14:14:07 -0500

    add mesa_control migrati3on

--- database/migrations/2026_04_20_131718_add_mesa_control_to_users_role.php
+++ database/migrations/2026_04_20_131718_add_mesa_control_to_users_role.php
@@ -1,24 +1,17 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
-        DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('admin', 'asesor', 'mesa_control')");
+        DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('admin', 'jefe', 'supervisor', 'asesor', 'mesa_control')");
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down()
-{
-    DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('admin', 'asesor')");
-}
+    {
+        DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('admin', 'jefe', 'supervisor', 'asesor')");
+    }
 };
